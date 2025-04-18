afb4c54399c9b09192db3f21943e220af6006cfd
Vinícius Miranda <viniciusmiranda97@outlook.com.br>
2025-04-18 14:07:32 +0100

    Update

--- HealthApp.Domain/EventBus/IEventBus.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HealthApp.Domain.EventBus;
-
-public interface IEventBus
-{
-    void Publish(string queueName, string message);
-    void Subscribe(string queueName, Action<string> handler);
-    void Dispose();
-}
